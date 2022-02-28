512b1681867b8a9be17d0f1dc75c2489a8ffc51e
Vladoochka <92115834+Vladoochka@users.noreply.github.com>
2022-02-28 17:30:01 +0300

    first commit

---
+++ test1.py
@@ -0,0 +1,14 @@
+def sum1():
+    print(num1 + num2)
+
+
+while True:
+    num1 = float(input("Введите первое число либо 0, если хотите завершить программу: "))
+    if num1 == 0:
+        break
+    num2 = float(input("Введите второе число: "))
+    sign = input("Введите +, -, *, /: ")
+    if sign == '+':
+        sum1()
+    else:
+        print("Введите +, -, *, /: ")
