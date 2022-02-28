da53814ecbd0c76a99e58a569616f6ed80e64e18
Vladoochka <92115834+Vladoochka@users.noreply.github.com>
2022-02-28 17:54:29 +0300

    added a try-except construct that checks division by 0

--- test1.py
+++ test1.py
@@ -14,19 +14,22 @@ def division():
     print(num1 / num2)
 
 
-while True:
-    num1 = float(input("Введите первое число либо 0, если хотите завершить программу: "))
-    if num1 == 0:
-        break
-    num2 = float(input("Введите второе число: "))
-    sign = input("Введите +, -, *, /: ")
-    if sign == '+':
-        sum1()
-    elif sign == '-':
-        subtraction()
-    elif sign == '*':
-        multiplication()
-    elif sign == '/':
-        division()
-    else:
-        print("Введите +, -, *, /: ")
+try:
+    while True:
+        num1 = float(input("Введите первое число либо 0, если хотите завершить программу: "))
+        if num1 == 0:
+            break
+        num2 = float(input("Введите второе число: "))
+        sign = input("Введите +, -, *, /: ")
+        if sign == '+':
+            sum1()
+        elif sign == '-':
+            subtraction()
+        elif sign == '*':
+            multiplication()
+        elif sign == '/':
+            division()
+        else:
+            print("Введите +, -, *, /: ")
+except ZeroDivisionError:
+    print("Деление на 0")
